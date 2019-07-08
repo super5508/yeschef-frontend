47d4a3e5598fb884ea65692e3a963daeef4d9922
super5508 <srdev5008@gmail.com>
2019-07-08 16:10:37 +0300

    add my profile skeaton (duplicated with signin branch)

---
+++ src/components/MyProfile.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+
+const styles = theme => ({
+    
+});
+
+class MyProfile extends Component {
+    render() {
+        const { classes } = this.props;
+        return (
+            <Box>
+
+            </Box>
+        );
+    }
+}
+
+export default withStyles(styles)(MyProfile);
