d165c5a66f74603f8e58ddf1c5fe663443e2d230
super5508 <srdev5008@gmail.com>
2019-07-08 19:58:34 +0500

    added signout component

---
+++ src/components/Settings.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import TextField from "@material-ui/core/TextField";
+import { withStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import { Button } from "@material-ui/core";
+import SignOut from "./SignOut";
+import UserIcon from "@material-ui/icons/Person";
+import Avatar from "@material-ui/core/Avatar";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import clsx from "clsx";
+
+const styles = theme => ({
+  setCon: {
+    display: "flex",
+    flexDirection: "row",
+    margin: "2rem 0rem 0rem 2.4rem",
+    "& h3": {
+      fontSize: "1.6rem",
+      fontWeight: 600,
+      marginTop: "0.5rem"
+    }
+  },
+  setImg: {
+    height: "5.7rem",
+    width: "5.7rem",
+    marginRight: "1rem"
+  },
+  pinkBg: {
+    backgroundColor: "#ff007f"
+  },
+  setEmail: {
+    fontSize: "1.4rem",
+    fontWeight: 300
+  },
+  userIcon: {
+    color: "#f2f2f2",
+    fontSize: "3.0rem"
+  }
+});
+
+class Settings extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { classes } = this.props;
+    console.log("name");
+    console.log(this.props.authStat);
+    return (
+      <Box>
+        {this.props.authStat && this.props.authStat.userProfile && (
+          <Box className={classes.setCon}>
+            {this.props.authStat.userProfile.photoURL ? (
+              <Avatar
+                alt={this.props.authStat.userProfile.displayName}
+                src={this.props.authStat.userProfile.photoURL}
+                className={classes.setImg}
+              />
+            ) : (
+              <Avatar
+                alt={this.props.authStat.userProfile.displayName}
+                className={clsx(classes.setImg, classes.pinkBg)}
+              >
+                <UserIcon className={classes.userIcon} />
+              </Avatar>
+            )}
+            <div>
+              <h3>
+                {this.props.authStat.userProfile.displayName}
+                <br />
+                <span className={classes.setEmail}>
+                  {this.props.authStat.userProfile.email}
+                </span>
+              </h3>
+            </div>
+          </Box>
+        )}
+        <SignOut history={this.props.history} />
+      </Box>
+    );
+  }
+}
+
+Settings.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    ...state.user
+  };
+};
+
+export default withRouter(
+  connect(mapStateToProps)(withStyles(styles)(Settings))
+);
--- src/pages/MyProfilePage.js
+++ src/pages/MyProfilePage.js
@@ -1,81 +1,82 @@
-import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
-import { withRouter, Link } from 'react-router-dom'
-import { connect } from 'react-redux'
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import { Paper } from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
-import SignIn from '../components/SignIn';
-import SignUp from '../components/SignUp';
-
-
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import { withRouter, Link } from "react-router-dom";
+import { connect } from "react-redux";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import { Paper } from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import SignIn from "../components/SignIn";
+import SignUp from "../components/SignUp";
+import Settings from '../components/Settings';
 
 const styles = theme => ({
-    signin_out_tabs:{
-        flexGrow: 1,
-        backgroundColor: "black",
-    },
-    tabs:{
-        borderBottom: '1px solid #8484836b'
-    },
-    h1:{
-        paddingTop:"2.4rem",
-        paddingLeft:"2.4rem",
-        paddingBottom:"0.3rem"
-    }
-
+  signin_out_tabs: {
+    flexGrow: 1,
+    backgroundColor: "black"
+  },
+  tabs: {
+    borderBottom: "1px solid #8484836b"
+  },
+  h1: {
+    paddingTop: "2.4rem",
+    paddingLeft: "2.4rem",
+    paddingBottom: "0.3rem"
+  }
 });
 class MyProfilePage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: "signup"
-        }
-
-    }
-    handleChange = (event, newValue) => {
-        this.setState({
-            ...this.state,
-            value: newValue
-        });
-    }
-
-    render() {
-        const { classes } = this.props;
-        return (
-            <Box>
-                <Typography className={classes.h1} variant="h1" component="h1">MY PROFILE</Typography>
-                {/* if user logged out */}
-                {!this.props.authStat.isLogin && (
-                    <Paper className={classes.signin_out_tabs}>
-                        <Tabs
-                            value={this.state.value}
-                            onChange={this.handleChange}
-                            indicatorColor="primary"
-                            textColor="inherit"
-                            centered
-                            className={classes.tabs}
-                            variant="fullWidth"
-                        >
-                            <Tab value="signup" label="Sign Up" />
-                            <Tab value="signin" label="Sign In" />
-                        </Tabs>
-                        {this.state.value === "signin"? <SignIn></SignIn> : <SignUp></SignUp>}
-                    </Paper>
-                ) }
-
-                {/* if user logged in */}
-                {this.props.authStat.isLogin && <div>settings component</div>}
-            </Box>
-        )
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "signup"
     };
+  }
+  handleChange = (event, newValue) => {
+    this.setState({
+      ...this.state,
+      value: newValue
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Box>
+        <Typography className={classes.h1} variant="h1" component="h1">
+          MY PROFILE
+        </Typography>
+        {/* if user logged out */}
+        {!this.props.authStat.isLogin && (
+          <Paper className={classes.signin_out_tabs}>
+            <Tabs
+              value={this.state.value}
+              onChange={this.handleChange}
+              indicatorColor="primary"
+              textColor="inherit"
+              centered
+              className={classes.tabs}
+              variant="fullWidth"
+            >
+              <Tab value="signup" label="Sign Up" />
+              <Tab value="signin" label="Sign In" />
+            </Tabs>
+            {this.state.value === "signin" ? <SignIn history={this.props.history} /> : <SignUp history={this.props.history} />}
+          </Paper>
+        )}
+
+        {/* if user logged in */}
+        {this.props.authStat.isLogin && <Settings history={this.props.history}/>}
+      </Box>
+    );
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
-    return {
-      ...state.user
-    }
-  }
-export default withRouter(connect(mapStateToProps)(withStyles(styles)(MyProfilePage)));
+  return {
+    ...state.user
+  };
+};
+export default withRouter(
+  connect(mapStateToProps)(withStyles(styles)(MyProfilePage))
+);
