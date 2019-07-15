0deea17331944e5035d7659f5e45cd0fa9795c93
super5508 <srdev5008@gmail.com>
2019-07-15 18:26:29 +0500

    removed extra line

--- src/App.js
+++ src/App.js
@@ -80,7 +80,6 @@ class App extends Component {
             <Route exact path="/" render={(routeProps) => (<HomePage />)}></Route>
             {/* <Route exact path="/" render={(routeProps) => (<ChefHomePage {...routeProps} {...chefsData}></ChefHomePage>)}></Route> */}
             <Route exact path="/terms-of-service" component={TermsOfService}></Route>
-            <Route exact path="/chef-home-page" component={ChefHomePage}></Route>
             <Route exact path="/privacy-policy" component={PrivacyPolicy}></Route>
             <Route exact path="/signin" component={SignIn}></Route>
             <Route exact path="/signout" component={SignOut}></Route>
