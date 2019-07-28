b563ce8dd581d8044379544a956bec0256336192
super5508 <srdev5008@gmail.com>
2019-07-28 13:37:47 +0300

    set axios timout 5000

--- src/common/AxiosMiddleware.js
+++ src/common/AxiosMiddleware.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === 'production') {
     console.log(Axios);
     instance = Axios.create({
         baseURL: 'https://us-central1-yeschef-7b155.cloudfunctions.net',
-        timeout: 2000
+        timeout: 5000
     });
 }
 export default instance;
